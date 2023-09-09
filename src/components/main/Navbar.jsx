064021caca8aaa9028f9466859352f460662b0e3
Gunay Zahra <126316477+gunayshakhmuradova@users.noreply.github.com>
2023-09-09 11:04:46 +0400

    Update Navbar.jsx

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <Link to="/">Gunay Shakhmuradova</Link>
+        <Link to="/homepage">Gunay Shakhmuradova</Link>
       </div>
       
       <nav>
         <ul>
           <li>
-            <Link to="/homepage">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/aboutme">About me</Link>
